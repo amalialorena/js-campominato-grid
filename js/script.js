64009886bc8cb06cb760f2eb110490e3cadf9b4a
Amalia <amalialorenaferariu@gmail.com>
2021-11-02 14:42:23 +0100

    pseudocodice

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+    //Creo dei prompt per far scegliere all'utente la difficoltà
+    //creo un if statement per generare la griglia giusta in base alla risposta dell'utente
+// in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+    //Creo una funzione per generare le celle => ciclo for
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (o simili, l’importante è dare all’utente il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).
+    //all'interno del ciclo for aggiungo un addEventListner click sui div quadrato
+        //al click il quadrato selezionato cambia colore
